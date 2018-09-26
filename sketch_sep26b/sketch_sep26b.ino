0b44e9f0bc06597195f833b5acdd5e6cf5b747b6
jennyhuoh <39110160+jennyhuoh@users.noreply.github.com>
2018-09-26 12:07:48 +0800

    Add files via upload

---
+++ sketch_sep26b/sketch_sep26b.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>  
+#include "SSD1306Wire.h"
+SSD1306Wire  display(0x3c, 21, 22);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(32,INPUT);
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    int rd;
+    rd=analogRead(32);
+    Serial.println(rd);
+    rd=map(rd,0,4095,0,255);
+    delay(7);
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 0, "Read Value is");
+    display.setFont(ArialMT_Plain_24);
+    display.drawString(0, 20, String(rd));
+    display.display();
+}
